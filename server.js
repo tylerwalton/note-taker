5a1df3f29e39cc0962429269cbe4fc75ea9b1b44
Tyler Walton <tylerwalton300@gmail.com>
2022-12-23 14:31:55 -0500

    correcting port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require("./apiRoutes");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
